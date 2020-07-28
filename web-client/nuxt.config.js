15b47f62e15909a614923ff71ca7393f3e5c4a09
sightro <tagir00123@gmail.com>
2020-07-28 10:44:11 +0300

    Move to Bootstrap-vue
    Why not? ¯\_(ツ)_/¯

--- web-client/nuxt.config.js
+++ web-client/nuxt.config.js
@@ -1,4 +1,3 @@
-import colors from 'vuetify/es5/util/colors'
 
 export default {
   /*
@@ -16,7 +15,6 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
@@ -47,12 +45,13 @@ export default {
   ** Nuxt.js dev-modules
   */
   buildModules: [
-    '@nuxtjs/vuetify',
   ],
   /*
   ** Nuxt.js modules
   */
   modules: [
+    // Doc: https://bootstrap-vue.js.org
+    'bootstrap-vue/nuxt',
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
   ],
@@ -62,27 +61,6 @@ export default {
   */
   axios: {
     baseURL: "http://localhost:5000"
-  },
-  /*
-  ** vuetify module configuration
-  ** https://github.com/nuxt-community/vuetify-module
-  */
-  vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
   },
   /*
   ** Build configuration
--- web-client/pages/index.vue
+++ web-client/pages/index.vue
@@ -1,9 +1,5 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
+  <div class="container">
     <div>
       <div v-if="quizzes">
         <p v-for="quiz in quizzes" :key="quiz.id">
@@ -12,30 +8,41 @@
       </div>
 
       <div>
-        <v-text-field label="Quiz name" v-model="quizName"/>
-        <v-btn @click="saveQuiz">
-          save
-        </v-btn>
+        <b-card class="container">
+          <b-card-title class="text-center">Quiz name</b-card-title>
+          <b-card-body class="row">
+            <b-input type="text" v-model="quizName"/>
+            <b-button @click="saveQuiz" class="col text-center mt-2">
+              save
+            </b-button>
+          </b-card-body>
+        </b-card>
       </div>
     </div>
-  </v-layout>
+  </div>
 </template>
 
 <script>
   import {mapMutations, mapActions, mapState} from 'vuex'
+
   export default {
+
     name: 'HomePage',
+
     data: () => ({
       quizName: ''
     }),
+
     computed: {
       ...mapState('quizzes', {
         quizzes: state => state.quizzes
       })
     },
+
     methods: {
       ...mapMutations('quizzes', ['reset', 'setQuizzes']),
       ...mapActions('quizzes', ['fetchQuizzes', 'createQuiz']),
+
       async saveQuiz() {
         await this.createQuiz({quiz: {name: this.quizName}});
         this.quizName = ''
@@ -43,3 +50,6 @@
     }
   }
 </script>
+
+<style>
+</style>
