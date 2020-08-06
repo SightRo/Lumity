4ec6e0fdad8ac919bfae670296dc58d6b57b8d7e
sightro <tagir00123@gmail.com>
2020-08-06 15:31:42 +0300

    Rewrite controller

---
+++ Lumity.Api/Controllers/QuizzesController.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lumity.Api.Data;
+using Lumity.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lumity.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class QuizzesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public QuizzesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Quiz>> GetAll()
+            => await _context.Quizzes.ToListAsync();
+
+        [HttpGet("{id}")]
+        public async Task<Quiz> GetById(int id)
+            => await _context.Quizzes.FindAsync(id);
+
+        [HttpPost]
+        public async Task<Quiz> Create([FromBody] Quiz quiz)
+        {
+            quiz.Created = DateTime.Now;
+            await _context.Quizzes.AddAsync(quiz);
+            await _context.SaveChangesAsync();
+            return quiz;
+        }
+
+        [HttpPut]
+        public async Task<Quiz?> Update([FromBody] Quiz quiz)
+        {
+            if (quiz.Id == 0)
+                return null;
+
+            quiz.LastModified = DateTime.Now;
+            _context.Quizzes.Update(quiz);
+            await _context.SaveChangesAsync();
+            return quiz;
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+            => throw new NotImplementedException();
+    }
+}
